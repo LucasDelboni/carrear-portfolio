a008b0a6602c437cd7bc8956fa6defd900e68e56
Lucas Delboni <lucas.delboni2@gmail.com>
2020-06-20 11:53:10 -0300

    adding theme provider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Contact from './section/Contact';
 import About from './section/About';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Career from './section/Career';
@@ -19,20 +19,30 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#000'
+    }
+  }
+});
+
 function App() {
-  const classes = useStyles();
+  const classes = useStyles(theme);
 
   return (
-    <div className={classes.background}>
-      <CssBaseline />
-      <Container className={classes.pageContent}>
-        <About />
-        <Skills />
-        <Career />
-        <Interests />
-        <Contact />
-      </Container>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div className={classes.background}>
+        <CssBaseline />
+        <Container className={classes.pageContent}>
+          <About />
+          <Skills />
+          <Career />
+          <Interests />
+          <Contact />
+        </Container>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
